754e2db256030c5e62623a213b97e3503c2c1937
Runze <ernuzwang@gmail.com>
2014-10-16 21:38:17 -0700

    added helper function; reorganized code

--- 3. crawl_goodreads.R
+++ 3. crawl_goodreads.R
@@ -2,6 +2,7 @@ library(rvest)
 library(plyr)
 library(stringr)
 options(stringsAsFactors = F)
+source('app/helper_functions.R')
 
 base_url = 'https://www.goodreads.com/list/show/264.Books_That_Everyone_Should_Read_At_Least_Once'
 base_ps = html(base_url)
@@ -32,8 +33,8 @@ for (p in 1:pgs) {
 gr_df$rank = 1:nrow(gr_df)
 save(gr_df, file = 'gr_df.RData')
 
-gr_df_s = subset(gr_df, rating >= 4)
-gr_df_s = subset(gr_df_s, rank <= quantile(rank, .5))
+gr_df_s = subset(gr_df, rating >= 4) #only keep books with a rating above 4
+gr_df_s = subset(gr_df_s, rank <= quantile(rank, .5)) #only keep the top 50 percentile
 save(gr_df_s, file = 'gr_df_s.RData')
 
 #check if these books are included in the nyt and npr list already
@@ -69,19 +70,11 @@ for (i in 1:nrow(gr_df_s)) {
     #check language
     lang = html_text(html_nodes(book_ps, '#bookDataBox :nth-child(3) .infoBoxRowItem'))
     if('English' %in% lang) {
-      genres = tryCatch(html_text(html_nodes(book_ps, '.elementList ')),
-                        error = function(cond) return(NA))
-      genre = paste(genres, collapse = '')
-      genre = gsub('-', '', genre)
-      genre = gsub('[^A-Za-z]', ' ', genre)
-      genre = gsub('users|Add |a |comment |comments ', ' ', genre)
-      genre = rm_space(gsub('Non Fiction', 'Nonfiction', genre))
+      #get genres
+      genres = extract_genre(book_ps, 'css')
       
-      descs = tryCatch(html_text(html_nodes(book_ps, '#description span')), 
-                       error = function(cond) return(NA))
-      desc = descs[which.max(sapply(descs, nchar))]
-      desc = gsub('-', '', desc)
-      desc = rm_space(desc)
+      #get descriptions
+      desc = extract_desc(book_ps, 'css')
       
       #get image
       img = html_attr(html_nodes(book_ps, '#coverImage'), 'src')
@@ -90,7 +83,7 @@ for (i in 1:nrow(gr_df_s)) {
       amazon = html_attr(html_nodes(book_ps, '.firstBuyButton .buttonBar'), 'href')
       amazon = paste0('https://www.goodreads.com', amazon)
       
-      gr = data.frame(rbind(gr, c(gr_df_s$title[i], gr_df_s$author[i], genre, desc, img, amazon))) 
+      gr = data.frame(rbind(gr, c(gr_df_s$title[i], gr_df_s$author[i], genres, desc, img, amazon))) 
     }
   }
 }
@@ -101,3 +94,4 @@ save(gr, file = 'gr.RData')
 #combine with nyt and npr
 nyt_npr_gr = data.frame(rbind.fill(nyt_npr, gr))
 save(nyt_npr_gr, file = 'nyt_npr_gr.RData')
+
